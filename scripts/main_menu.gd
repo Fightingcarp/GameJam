d7e79e8568da17dad8e373c28b0a11faf89ea0ea
Fightingcarp <122738768+Fightingcarp@users.noreply.github.com>
2024-08-19 00:33:28 +0800

    removed options buttons

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _on_start_pressed():
 	get_tree().change_scene_to_file("res://scenes/level_select.tscn")
 
 func _on_settings_pressed():
-	get_tree().change_scene_to_file("res://scenes/settings_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/credits_menu.tscn")
 
 func _on_exit_pressed():
 	get_tree().quit()
